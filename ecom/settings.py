aa2588b6283ac469d1f0021201af5f6bbff3d300
Amjad PV <amjadpvamd10@gmail.com>
2024-11-21 19:03:47 +0530

    updated 7:03pm

--- ecom/settings.py
+++ ecom/settings.py
@@ -129,9 +129,6 @@ STATICFILES_DIRS = ['static/']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
