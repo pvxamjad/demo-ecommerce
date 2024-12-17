b549a6669570a34303dae71f107e252d66f8e3f0
Amjad P V <amjadpvamd10@gmail.com>
2024-12-17 15:35:01 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-@45-l$013syw$yv0g-z1e-onfhka!%esv-y-o(t_())ubtlmzn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = [*]
 
 
 
